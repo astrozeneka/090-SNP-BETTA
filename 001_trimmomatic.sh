f1b69f59f5d4ce10f14d17b22aee7848a63df732
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-03-29 16:07:19 +0700

    Initial comit

---
+++ 001_trimmomatic.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -N 1 -c 96
+#SBATCH -t 48:00:00
+#SBATCH -J trimmomatic
+#SBATCH -A proj5034
+
+conda activate /tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/conda-envs/trimmomatic
+
+genome=(
+  "ERR3332434"
+  "ERR3332435"
+  "ERR3332436"
+  "ERR3332437"
+  "SRR18231392"
+  "SRR18231393"
+  "SRR18231394"
+  "SRR18231395"
+  "SRR18231396"
+  "SRR18231397"
+)
+
+READDIR="/tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/betta-cleaned/reads"
+ILLUMINACLIP="/tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/conda-envs/trimmomatic/share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa"
+mkdir -p data/trimmed_reads
+for genome in "${genomes[@]}"; do
+  echo "Trimming $genome"
+  trimmomatic PE -phred33 "${READDIR}:${genome}_1.1.fq.gz" "${READDIR}:${genome}_1.2.fq.gz" \
+    data/trimmed_reads/${genome}_1.trimmed.fq.gz data/trimmed_reads/${genome}_1.unpaired.fq.gz \
+    data/trimmed_reads/${genome}_2.trimmed.fq.gz data/trimmed_reads/${genome}_2.unpaired.fq.gz \
+    ILLUMINACLIP:${ILLUMINACLIP}:2:30:10 \
+    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+done
---
+++ readme.md
@@ -0,0 +1,2 @@
+# SNP analysis
+Correct the MAP pipeline in order to get the correct BAM files
