d3a5667fd2036b3b14124a03aa3aa51a0a1d38b9
Ryan R <rasoarahonarivoniaina.h@ku.th>
2024-03-29 16:14:54 +0700

    update 001 to be run on the server

--- 001_trimmomatic.sh
+++ 001_trimmomatic.sh
@@ -5,9 +5,10 @@
 #SBATCH -J trimmomatic
 #SBATCH -A proj5034
 
+source ~/.bashrc
 conda activate /tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/conda-envs/trimmomatic
 
-genome=(
+genomes=(
   "ERR3332434"
   "ERR3332435"
   "ERR3332436"
@@ -25,7 +26,7 @@ ILLUMINACLIP="/tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/conda-envs/trimmom
 mkdir -p data/trimmed_reads
 for genome in "${genomes[@]}"; do
   echo "Trimming $genome"
-  trimmomatic PE -phred33 "${READDIR}:${genome}_1.1.fq.gz" "${READDIR}:${genome}_1.2.fq.gz" \
+  trimmomatic PE -phred33 "${READDIR}/${genome}_1.1.fq.gz" "${READDIR}/${genome}_2.2.fq.gz" \
     data/trimmed_reads/${genome}_1.trimmed.fq.gz data/trimmed_reads/${genome}_1.unpaired.fq.gz \
     data/trimmed_reads/${genome}_2.trimmed.fq.gz data/trimmed_reads/${genome}_2.unpaired.fq.gz \
     ILLUMINACLIP:${ILLUMINACLIP}:2:30:10 \
