15d1acd3f3be2d6ee85de773e97d682759b8c79e
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-04-04 16:01:53 +0700

    Update the python script 10

--- python/10_convert_depth_to_window.py
+++ python/10_convert_depth_to_window.py
@@ -9,14 +9,24 @@ if __name__ == '__main__':
     # load from stdin
     window_depth = 0
     current_window = -1
+    current_chr = None
     for line in sys.stdin:
         line = line.strip().split()
         window_id = (int(line[1]) // args.window) * args.window
+        chr = line[0]
+
+        if chr != current_chr:
+            if current_chr != None:
+                window_depth = (window_depth / args.window)
+                print(current_chr, current_window, window_depth)
+            current_chr = chr
+            current_window = window_id
+            window_depth = 0
 
         if window_id != current_window:
             if current_window != -1:
                 window_depth = (window_depth / args.window)
-                print(current_window, window_depth)
+                print(current_chr, current_window, window_depth)
             current_window = window_id
             window_depth = 0
         window_depth += int(line[2])
