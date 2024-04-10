8a252a4bfc17d240e62765c70f61d878bd10f2c3
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-04-10 10:50:49 +0700

    Add 503

--- 503_gstacks.sh
+++ 503_gstacks.sh
@@ -11,25 +11,25 @@ export PATH=$PATH:/tarafs/data/project/proj5034-AGBKU/local/bin/
 cat <<EOF > popmap.txt
 ERR3332435.sorted	male
 ERR3332436.sorted	male
-SRR18231392.sorted male
-SRR18231393.sorted male
-SRR18231394.sorted male
-SRR18231395.sorted male
-SRR18231396.sorted male
-SRR18231397.sorted male
-SRR18231399.sorted male
-SRR18231401.sorted female
-SRR18231402.sorted female
-SRR18231403.sorted male
-SRR18231404.sorted female
-SRR18231405.sorted female
-SRR18231406.sorted male
-SRR18231407.sorted female
-SRR18231408.sorted female
-SRR18231409.sorted male
-SRR18231412.sorted female
-SRR18231415.sorted female
-SRR18231416.sorted female
+SRR18231392.sorted	male
+SRR18231393.sorted	male
+SRR18231394.sorted	male
+SRR18231395.sorted	male
+SRR18231396.sorted	male
+SRR18231397.sorted	male
+SRR18231399.sorted	male
+SRR18231401.sorted	female
+SRR18231402.sorted	female
+SRR18231403.sorted	male
+SRR18231404.sorted	female
+SRR18231405.sorted	female
+SRR18231406.sorted	male
+SRR18231407.sorted	female
+SRR18231408.sorted	female
+SRR18231409.sorted	male
+SRR18231412.sorted	female
+SRR18231415.sorted	female
+SRR18231416.sorted	female
 EOF
 
 SLUG=$1
