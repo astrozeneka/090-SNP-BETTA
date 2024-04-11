2f106199326c14ab5cecbc48c74cfb4630a7da19
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-04-11 09:42:58 +0700

    Add 505 for genotype imputation

---
+++ 505_beagle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -N 1 -c 4
+#SBATCH -t 24:00:00
+#SBATCH --mem=100GB
+#SBATCH -J beagle
+#SBATCH -A proj5057
+
+module load BCFtools
+
+SLUG=$1
+# Reorder the vcf file by using BCFTools sort
+bcftools sort data/gstacks-${SLUG}/populations.snps.vcf -o data/gstacks-${SLUG}/populations.snps.sorted.vcf
+
+# Remove the duplicates using our custom python script
+cat data/gstacks-${SLUG}/populations.snps.sorted.vcf | python python/105_filter_data.py > data/gstacks-${SLUG}/populations.snps.sorted.rmdup.vcf
+
+# Impute missing genotypes using Beagle
+java -jar /tarafs/data/home/hrasoara/softwares/beagle.22Jul22.46e.jar \
+	gt=data/gstacks-${SLUG}/populations.snps.sorted.rmdup.vcf \
+	out=data/gstacks-${SLUG}/populations.snps.imputed.vcf
+
+echo "Genotype imputation done"
