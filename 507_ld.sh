582285df9611a3e4d58e353448fcd88f6b179125
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2024-04-11 11:17:21 +0700

    Add 507 for calculating LD

---
+++ 507_ld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p memory
+#SBATCH -N 1 -c 32
+#SBATCH -t 24:00:00
+#SBATCH --mem=100GB
+#SBATCH -J plink-ld
+#SBATCH -A proj5057
+
+SLUG=$1
+
+/tarafs/data/home/hrasoara/proj5057-AGBKUB/ryan/Softwares/plink \
+	--bfile data/gstacks-${SLUG}/sex \
+	--r \
+	--ld-window-r2 0.2 \
+	--ld-window 10 \
+	--ld-window-kb 1000 \
+	--out data/gstacks-${SLUG}/sex
+echo "PLINK LD done"
